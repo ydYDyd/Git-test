b2de80e827e2c326b1ca0b405e573f61b6774dcb
yd <185096025@qq.com>
2021-07-16 22:30:47 +0800

    push test

--- src/main/java/com/yd/gitClass/GitTest.java
+++ src/main/java/com/yd/gitClass/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello word");
         System.out.println("master");
         System.out.println("yd-test");
+        System.out.println("push");
     }
 }
