86ec4ce8a0229d1571a41a4aa61a55e0fbe8757f
yd <185096025@qq.com>
2021-07-16 21:57:05 +0800

    yd-test first

--- src/main/java/com/yd/gitClass/GitTest.java
+++ src/main/java/com/yd/gitClass/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
