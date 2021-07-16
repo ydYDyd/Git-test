e892406a4f3505ec89f52c360b94467c1433c44d
yd <185096025@qq.com>
2021-07-16 21:41:24 +0800

    git second

--- src/main/java/com/yd/gitClass/GitTest.java
+++ src/main/java/com/yd/gitClass/GitTest.java
@@ -3,5 +3,6 @@ package com.yd.gitClass;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
